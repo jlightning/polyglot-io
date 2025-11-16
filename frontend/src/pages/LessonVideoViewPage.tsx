d01d8cc3d5ad5612ab15ccbbfdb4f423a5f84d88
James Tran <james@thetreedots.com>
2025-11-16 16:57:18 +0800

    feat: improve video lesson page layout

--- frontend/src/pages/LessonVideoViewPage.tsx
+++ frontend/src/pages/LessonVideoViewPage.tsx
@@ -935,7 +935,7 @@ const LessonVideoViewPage: React.FC = () => {
           style={{
             width: '100%',
             height: '100%',
-            maxHeight: '1000px',
+            maxHeight: '950px',
             borderRadius: '8px',
             objectFit: 'contain',
           }}
